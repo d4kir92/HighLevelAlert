721c654e241b22fe339a5df4f763edad1f34849f
D4KiR <d4kir92@gmail.com>
2023-04-21 18:33:17 +0200

    v0.2.1

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -10,7 +10,7 @@ function HighLevelAlert:LangdeDE()
 	LHLA["LID_SKULLELITESNEARBY"] = "%d {rt8}-Elites in der Nähe!"
 	LHLA["LID_SKULLSNEARBY"] = "%d {rt8} in der Nähe!"
 	LHLA["LID_REDELITESNEARBY"] = "%d Rot-Eliten in der Nähe!"
-	LHLA["LID_REDSNEARBY"] = "%d Rot-Eliten in der Nähe!"
+	LHLA["LID_REDSNEARBY"] = "%d Rote in der Nähe!"
 	LHLA["LID_HELPTEXTLOCKED"] = "Warnhinweis-Text ist fixiert, Rechtsklick zum Entsperren."
 	LHLA["LID_LOCKEDTEXT"] = "Warnhinweis-Text ist jetzt gesperrt."
 	LHLA["LID_UNLOCKEDTEXT"] = "Warnhinweis-Text ist jetzt entsperrt."
