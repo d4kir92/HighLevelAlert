2a4ffc6558acfb5f3f459a4e37847e6c76c64ae6
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-07-19 07:09:48 +0200

    v0.4.36

--- settings.lua
+++ settings.lua
@@ -13,14 +13,14 @@ end
 
 function HighLevelAlert:InitSettings()
     HLATAB = HLATAB or {}
-    HighLevelAlert:SetVersion(AddonName, 136219, "0.4.35")
+    HighLevelAlert:SetVersion(AddonName, 136219, "0.4.36")
     hla_settings = HighLevelAlert:CreateFrame(
         {
             ["name"] = "HighLevelAlert",
             ["pTab"] = {"CENTER"},
             ["sw"] = 520,
             ["sh"] = 520,
-            ["title"] = format("HighLevelAlert |T136219:16:16:0:0|t v|cff3FC7EB%s", "0.4.35")
+            ["title"] = format("HighLevelAlert |T136219:16:16:0:0|t v|cff3FC7EB%s", "0.4.36")
         }
     )
 
