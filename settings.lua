62daa3b5648ccc68427e0c03b853e2baa5f918a8
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-11-20 12:43:29 +0100

    v0.4.46

--- settings.lua
+++ settings.lua
@@ -13,14 +13,14 @@ end
 
 function HighLevelAlert:InitSettings()
     HLATAB = HLATAB or {}
-    HighLevelAlert:SetVersion(AddonName, 136219, "0.4.45")
+    HighLevelAlert:SetVersion(AddonName, 136219, "0.4.46")
     hla_settings = HighLevelAlert:CreateFrame(
         {
             ["name"] = "HighLevelAlert",
             ["pTab"] = {"CENTER"},
             ["sw"] = 520,
             ["sh"] = 520,
-            ["title"] = format("HighLevelAlert |T136219:16:16:0:0|t v|cff3FC7EB%s", "0.4.45")
+            ["title"] = format("HighLevelAlert |T136219:16:16:0:0|t v|cff3FC7EB%s", "0.4.46")
         }
     )
 
@@ -87,7 +87,7 @@ function HighLevelAlert:InitSettings()
             ["name"] = "HighLevelAlert",
             ["icon"] = 136219,
             ["dbtab"] = HLATAB,
-            ["vTT"] = {{"HighLevelAlert |T136219:16:16:0:0|t", "v|cff3FC7EB0.4.45"}, {"Leftclick", "Toggle Settings"}, {"Rightclick", "Unlock/lock Text"}, {"Shift + Rightclick", "Hide Minimap Icon"}},
+            ["vTT"] = {{"HighLevelAlert |T136219:16:16:0:0|t", "v|cff3FC7EB0.4.46"}, {"Leftclick", "Toggle Settings"}, {"Rightclick", "Unlock/lock Text"}, {"Shift + Rightclick", "Hide Minimap Icon"}},
             ["funcL"] = function()
                 HighLevelAlert:ToggleSettings()
             end,
