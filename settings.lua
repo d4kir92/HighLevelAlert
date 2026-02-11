1e228232e5f794d63094d331dcace3df22de9129
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-11 13:57:33 +0100

    v0.4.71

--- settings.lua
+++ settings.lua
@@ -13,7 +13,7 @@ end
 
 function HighLevelAlert:InitSettings()
     HLATAB = HLATAB or {}
-    HighLevelAlert:SetVersion(136219, "0.4.70")
+    HighLevelAlert:SetVersion(136219, "0.4.71")
     hla_settings = HighLevelAlert:CreateWindow(
         {
             ["name"] = "HighLevelAlert",
